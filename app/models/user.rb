f68cde7036e59c7cce741e680dbfe513922450a7
shioripika <pikashu.get.daze.ww@gmail.com>
2020-08-20 11:10:40 +0000

    like_add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :likes, dependent: :destroy
+ 
   attr_accessor :remember_token
   before_save {email.downcase!}
   validates :name, presence: true, length: {maximum: 30}
